cf1da286bd2cdc8561710c92be6898bd7a44d246
usernaimandrey <anshlyapnikov@yandex.ru>
2022-04-25 04:44:56 +0300

    initcomit

---
+++ server/src/orders/orders.resolver.ts
@@ -0,0 +1,20 @@
+import { Args, Query, Resolver } from '@nestjs/graphql'
+import { RetailService } from '../retail_api/retail.service'
+import { OrdersResponse } from '../graphql'
+
+@Resolver('Orders')
+export class OrdersResolver {
+  constructor(private retailService: RetailService) {}
+
+  @Query()
+  async order(@Args('number') id: string): Promise<any> {
+    const order = await this.retailService.findOrder(id)
+    return order
+  }
+
+  @Query()
+  async getOrders(@Args('page') page: number): Promise<OrdersResponse> {
+    const [orders, pagination] = await this.retailService.orders({ page: page })
+    return { orders: orders, pagination: pagination }
+  }
+}
